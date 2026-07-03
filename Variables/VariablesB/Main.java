efd64413c4264d6055db6691504f39b57bdb3552
Ivan <ivanperdomo0819@gmail.com>
2026-07-03 19:48:06 -0400

    Started the assignment and will continue it later

--- Variables/VariablesB/Main.java
+++ Variables/VariablesB/Main.java
@@ -1,4 +1,11 @@
 package Variables.VariablesB;
+import java.util.Scanner;
+
 public class Main {
-    
+    int radical;
+    int regetni;
+
+    System.out.println();
+    System.out.println();
+
 }
