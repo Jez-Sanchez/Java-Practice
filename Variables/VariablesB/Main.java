46ba0e33696e4155bb19c4bd2e3b0dc57e732af5
Anaya-108 <sukir1738@gmail.com>
2026-07-09 20:02:59 -0400

    Update Main.java

--- Variables/VariablesB/Main.java
+++ Variables/VariablesB/Main.java
@@ -1,4 +1,24 @@
 package Variables.VariablesB;
+import java.util.Scanner;
 public class Main {
+ public static void main(String[] args ) {
+
+// scanner
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter your first integer");
+int number1 = scanner.nextInt();
+System.out.println("Enter your second integer");
+int number2 = scanner.nextInt();
+
+// print statements
+System.out.println(number1 + number2);
+System.out.println(number1 - number2);
+System.out.println(number1 * number2);
+System.out.println(number1 / number2);
+System.out.println(number1 % number2);
+
+scanner.close(); 
+   }
+ }
     
 }
