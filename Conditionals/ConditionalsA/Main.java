e42cb59f755a691981be514cc14668bda26ae9d5
Anaya-108 <sukir1738@gmail.com>
2026-07-13 21:18:15 -0400

    Update Main.java

--- Conditionals/ConditionalsA/Main.java
+++ Conditionals/ConditionalsA/Main.java
@@ -1,4 +1,23 @@
 package Conditionals.ConditionalsA;
+import java.util.Scanner;
 public class Main{
+public static void main(String[] args) {
+  
+// scanner
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter voltage number");
+double voltage = scanner.nextDouble();
+  
+// conditionals
+if (voltage >= 12.0){
+  System.out.println("Battery High.");
+} else if (voltage >= 9.5) {
+  System.out.println("Battery Mid.");
+} else if (voltage < 9.5) {
+  System.out.println("Battery Low.");
 
+ scanner.close();
+
+  } 
+ }
 }
