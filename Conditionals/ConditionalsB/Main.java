3ca7a1573e5ac1f1aac9350800d00ac5babe6b3c
Anaya-108 <sukir1738@gmail.com>
2026-07-22 12:18:07 -0400

    Update Main.java

--- Conditionals/ConditionalsB/Main.java
+++ Conditionals/ConditionalsB/Main.java
@@ -1,5 +1,28 @@
 package Conditionals.ConditionalsB;
 
-public class Main {
-    
-}
+import java.util.Scanner; 
+public class Main { 
+public static final double distanceValue = 4.5;
+public static void main(String[] args) {
+
+// scanner
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter Hood Angle");
+double hoodAngle = scanner.nextDouble();
+System.out.println("Enter Game Piece Speed");
+int rpm = scanner.nextInt();
+scanner.close();
+
+// equation
+double distance = (0.002626 * rpm) * Math.cos(Math.toRadians(hoodAngle));
+
+// conditionals
+if (distance > 4.5) {
+System.out.println("The ball has overshot!");
+} else if (distance == 4.5) {
+System.out.println("The ball has been scored!");
+} else {
+System.out.println("The ball has undershot!");
+     }
+   }
+ }
