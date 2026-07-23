cc90ca69205ad31b2f797663ebcb7dfe2e02216b
Anaya-108 <sukir1738@gmail.com>
2026-07-23 11:47:49 -0400

    Update Main.java

--- Conditionals/SwitchCases/Main.java
+++ Conditionals/SwitchCases/Main.java
@@ -1,5 +1,27 @@
 package Conditionals.SwitchCases;
-
-public class Main {
-    
-}
+import java.util.Scanner;
+public class main {
+public static void main(String [] args) {
+// scanner
+Scanner scanner = new Scanner(System.in);
+System.out.println("Select a command.");
+String command = scanner.nextLine();
+scanner.close();
+// switch statement
+switch (command) {
+ case "Drive":
+ System.out.println("Driving!");
+ break;
+ case "Intake":
+ System.out.println("Intaking!");
+ break;
+ case "Shoot":
+ System.out.println("Shooting!");
+ break;
+ case "Climb":
+ System.out.println("Climbing!");
+ default:
+ System.out.println("Unknown command D:");
+        }
+     }
+  }
