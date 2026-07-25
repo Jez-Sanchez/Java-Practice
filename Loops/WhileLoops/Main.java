d8032fc083ec6f27ece0c00a992a7786238de24d
Anaya-108 <sukir1738@gmail.com>
2026-07-25 16:33:45 -0400

    Update Main.java
    the code works but breaks if you enter 5 and doesn't check for any false statements..

--- Loops/WhileLoops/Main.java
+++ Loops/WhileLoops/Main.java
@@ -1,5 +1,44 @@
-package Loops.WhileLoopsA;
-
+import java.util.Scanner;
 public class Main {
-    
+public static void main(String[] args) {
+
+boolean userContinue = true;
+String location = "Goal";
+boolean haveGamePieces = false;
+Scanner scanner = new Scanner(System.in);
+
+while (userContinue) {
+System.out.println("Choose a command: 1.Drive 2.Intake 3.Shoot 4.Climb 5.Shut Down");
+int command = scanner.nextInt();
+
+// code works fine, but doesn't trigger anything that is false whatsover..
+switch (command) {
+case 1:
+if (haveGamePieces) {
+System.out.println("Drive to game pieces!");
+} else System.out.println("Driving to goal location..");
+System.out.println("Arrived at goal!"); 
+break;
+case 2:
+if (location.equals("Goal")) { 
+System.out.println("Intaking..");
+System.out.println("Acquired Fuel!");
+} else System.out.println("Cannot intake..acquire fuel!"); 
+break;
+case 3:
+if (haveGamePieces) {
+System.out.println("Cannot shoot..");
+} else if (location.equals("Goal")) {
+System.out.println("Shooting!");
+} else System.out.println("Cannot shoot..");
+break;
+case 4:
+System.out.println("Climbing!");
+break;
+case 5:
+System.out.println("Shutting Down!");
+scanner.close(); // this causes the code to break...not sure why nor how to fix D:
+       }
+    }
+  }
 }
