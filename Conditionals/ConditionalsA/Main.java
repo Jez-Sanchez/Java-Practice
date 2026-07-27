b1fefe5f2cb885d2ac41c8c4a96817053b828e2f
Ivan <ivanperdomo0819@gmail.com>
2026-07-27 16:25:21 -0400

    I have finished the exercise however I am trying to see why the files aren't running correctly

--- Conditionals/ConditionalsA/Main.java
+++ Conditionals/ConditionalsA/Main.java
@@ -1,4 +1,21 @@
 package Conditionals.ConditionalsA;
+import java.util.Scanner;
 public class Main{
+public static void main(String[]args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your voltage number");
+    double voltage = scanner.nextDouble();
+
+    scanner.close();
+}
+
+    if (voltage >= 12.0) {
+System.out.println("Batterry High");
+    } else if (voltage >= 9.5) {
+System.out.println("Battery Mid");
+    } else {
+System.out.println("Battery Low");
+    }
 
 }
