5bc54c98338b5e518926ed3e351199d96dc20a30
Ivan <ivanperdomo0819@gmail.com>
2026-07-29 14:39:57 -0400

    I have done the assigment and will check how the scanner class works again

--- Conditionals/SwitchCases/Main.java
+++ Conditionals/SwitchCases/Main.java
@@ -1,5 +1,28 @@
 package Conditionals.SwitchCases;
+import java.util.Scanner;
 
 public class Main {
-    
+public static void main(String[]args) {
+ Scanner scanner = new Scanner(System.in);
+ System.out.println("Select an option");   
+ String option = scanner.nextLine();
+ scanner.close();
+}
+
+switch (option){
+    case "Drive":
+    System.out.println("Driving...");
+    break;
+    case "Intake":
+    System.out.println("Intaking...");
+    break;
+    case "Shoot":
+    System.out.println("Shooting...");
+    break;
+    case"Climb":
+    System.out.println("Climbing...");
+    break;
+    default:
+    System.out.println("Option not found, please try again");
+    }
 }
