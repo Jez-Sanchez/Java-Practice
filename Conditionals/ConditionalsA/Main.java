b840c7983dd32ee709c925eced83355e832e3852
Nik-the-Dex <Nikohawk77@gmail.com>
2026-07-29 23:00:59 -0400

    finished Conditionals

--- Conditionals/ConditionalsA/Main.java
+++ Conditionals/ConditionalsA/Main.java
@@ -1,4 +1,23 @@
 package Conditionals.ConditionalsA;
-public class Main{
 
-}
+import java.util.Scanner;
+
+ 
+public class Main{
+// replace voltage with main
+public static void /*voltage*/ main(String[] args) {
+ Scanner mrohm = new Scanner(System.in);
+ System.out.print("input your voltage: ");
+ double resist = mrohm.nextDouble();
+
+    if(resist >= 12.0) {
+         System.out.println("high");
+     } else if(resist >= 9.5){
+         System.out.println("med");
+    } else{
+         System.out.println("low");
+}; mrohm.close();
+
+}}
+
+
--- Conditionals/ConditionalsB/Main.java
+++ Conditionals/ConditionalsB/Main.java
@@ -1,5 +1,29 @@
 package Conditionals.ConditionalsB;
-
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args){
+Scanner balls = new Scanner(System.in); // i can name it whatever i want HAHAHAHA
+
+final double goalPlace = 4.5;
+System.out.print("enter your angle: ");
+double hoodAngle = balls.nextDouble();
+
+System.out.print("enter your Rpm: ");
+double Rpm = balls.nextDouble();
+
+
+double distance = (0.002626 * Rpm) * Math.cos(Math.toRadians(hoodAngle));
+System.out.printf("balls travel distance: ", distance);
+
+if (distance>goalPlace) {
+    System.out.println("overshot :(");
+} else if (distance == goalPlace) {
+    System.out.println("GOALLLLLLLLLLLLLLLLLL");
+} else {
+    System.out.println("undershot :,(");
+}
+balls.close();
+
+    }
     
 }
