44830b56e9aad3f07b2d227fd9d64eaf819bc64c
Isaiah Isom <isomisaiah93@gmail.com>
2026-08-19 09:06:56 -0400

    Update Main.java

--- Variables/VariablesA/Main.java
+++ Variables/VariablesA/Main.java
@@ -1,9 +1,13 @@
 public class Main{
     public static void main(String[] args) {
-        string teamName = SigmaC@T Robotics Team;
-        int teamNumber = 108
-        int batteryVoltage = 12.6;
-        areWeTheOldestTeamInFlorida = ;
+        string teamName = "SigmaC@T Robotics Team";
+        int teamNumber = 108;
+        double batteryVoltage = 12.6;
+        boolean areWeTheOldestTeamInFlorida = true;
+        System.out.println(teamName);
+        System.out.println(teamNumber);
+        System.out.println(batteryVoltage);
+        System.out.println(areWeTheOldestTeamInFlorida);
 
         //Print all of these variables below
     }
