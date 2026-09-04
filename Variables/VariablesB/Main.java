caaabfb4f58746d3eba8d2bf11f8f7567045ffdd
Isaiah Isom <isomisaiah93@gmail.com>
2026-09-04 08:04:31 -0400

    Update Main.java

--- Variables/VariablesB/Main.java
+++ Variables/VariablesB/Main.java
@@ -1,5 +1,11 @@
 package Variables.VariablesB;
 import java.util.Scanner;
 public class Main {
-    
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 7;
+        Scanner sc = new Scanner(System.in);
+        
+        
+    }
 }
