40fe5e41fed07423a5e1320c940e0e99fc4b6b63
Isaiah Isom <isomisaiah93@gmail.com>
2026-09-04 08:50:37 -0400

    Update Main.java

--- Conditionals/ConditionalsB/Main.java
+++ Conditionals/ConditionalsB/Main.java
@@ -1,5 +1,17 @@
 package Conditionals.ConditionalsB;
-
+import java.util.Scanner;
 public class Main {
-    
+    public static void main(String[] args) {
+        final double distance = 4.5;
+        Scanner sc = new Scanner(System.in);
+        distance = (0.002626 * RPM) * Math.cos(Math.toRadians(HoodAngle));
+        System.out.println("Enter the Hood Angle (In degress)");
+        sc.nextInt();
+        System.out.println("Enter the Game Piece Speed);
+        sc.nextInt();
+        if (distance > 4.5) {
+            System.out.println("The ball overshoots");
+            if (
+        }
+    }
 }
